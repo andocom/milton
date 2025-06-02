4726dddc70a9d8d7b6283008abf05d8ec36f42e6
andocom <91099431+andocom@users.noreply.github.com>
2025-06-02 15:30:03 +1000

    Added real email sended for auth

---
+++ milton/Components/Account/EmailSender.cs
@@ -0,0 +1,61 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using milton.Data;
+using MimeKit;
+
+namespace milton.Components.Account
+{
+    internal sealed class EmailSender : IEmailSender<ApplicationUser>
+    {
+        private readonly EmailSettings _settings;
+
+        public EmailSender(EmailSettings settings)
+        {
+            _settings = settings;
+        }
+
+        public async Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink)
+        {
+            var subject = "Confirm your email";
+            var htmlMessage = $"Please confirm your account by <a href='{confirmationLink}'>clicking here</a>.";
+            await SendEmailAsync(email, subject, htmlMessage);
+        }
+
+        public async Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink)
+        {
+            var subject = "Reset your password";
+            var htmlMessage = $"Reset your password by <a href='{resetLink}'>clicking here</a>.";
+            await SendEmailAsync(email, subject, htmlMessage);
+        }
+
+        public async Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode)
+        {
+            var subject = "Reset your password";
+            var htmlMessage = $"Please reset your password using the following code: {resetCode}";
+            await SendEmailAsync(email, subject, htmlMessage);
+            //throw new NotImplementedException();
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var message = new MimeMessage();
+            message.From.Add(MailboxAddress.Parse(_settings.SenderEmail));
+            message.To.Add(MailboxAddress.Parse(email));
+            message.Subject = subject;
+
+            var bodyBuilder = new BodyBuilder { HtmlBody = htmlMessage };
+            message.Body = bodyBuilder.ToMessageBody();
+
+            using var client = new SmtpClient();
+            await client.ConnectAsync(_settings.SmtpServer, _settings.Port, SecureSocketOptions.StartTls);
+            await client.AuthenticateAsync(_settings.Username, _settings.Password);
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+        }
+
+        
+    }
+}
+
---
+++ milton/Components/Account/EmailSettings.cs
@@ -0,0 +1,8 @@
+﻿public class EmailSettings
+{
+    public string SmtpServer { get; set; } = "smtp.office365.com";
+    public int Port { get; set; } = 587;
+    public string SenderEmail { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
